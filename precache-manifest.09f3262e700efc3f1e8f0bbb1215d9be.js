f827fe46a4c904eca4b13df678fd0ae7840907d5
Ansari Al Nawaz <alnawaz1231@gmail.com>
2022-10-26 11:58:25 +0530

    Updates

---
+++ precache-manifest.09f3262e700efc3f1e8f0bbb1215d9be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3aca2effd033a434e4b0",
+    "url": "/formsample/static/css/main.69614154.chunk.css"
+  },
+  {
+    "revision": "3aca2effd033a434e4b0",
+    "url": "/formsample/static/js/main.3aca2eff.chunk.js"
+  },
+  {
+    "revision": "f3b84ad1a4a60d245526",
+    "url": "/formsample/static/js/1.f3b84ad1.chunk.js"
+  },
+  {
+    "revision": "bb37355b0a55eb244d87",
+    "url": "/formsample/static/js/runtime~main.bb37355b.js"
+  },
+  {
+    "revision": "21479d326ad1a3db5e7c25499809d145",
+    "url": "/formsample/index.html"
+  }
+];
